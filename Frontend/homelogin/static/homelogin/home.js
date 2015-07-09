3bab28c9afd7a66b4296be44f6d8bcda2aadd342
Daniel Bolink <dabolink@uvic.ca>
2015-07-09 12:11:52 -0700

    forgot to remove a static var

--- Frontend/homelogin/static/homelogin/home.js
+++ Frontend/homelogin/static/homelogin/home.js
@@ -10,7 +10,7 @@ function toggleBot(event, username){
 
     }else{
     	var xmlhttp = new XMLHttpRequest();
-    	xmlhttp.open("GET", "http://localhost:5000/dabolinkbot/api/v1.0/bot/end/"+username);
+    	xmlhttp.open("GET", backend_server_ip + "/dabolinkbot/api/v1.0/bot/end/"+username);
     	xmlhttp.send();
         event.target.className = "btn btn-lg btn-danger";
         event.target.innerHTML = "Offline";
