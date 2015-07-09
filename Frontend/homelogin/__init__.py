ac8a8f0a6c73a9c9c14cb9a9ada4fa66c2d60bfa
Daniel Bolink <dabolink@uvic.ca>
2015-07-09 09:08:28 -0700

    made it dynamic

---
+++ Frontend/homelogin/__init__.py
@@ -0,0 +1,6 @@
+fname = "values.txt"
+d = dict()
+with open(fname) as f:
+    for line in f:
+        parsed = line.split(" ")
+        d[parsed[0]] = parsed[1]
--- Frontend/homelogin/views.py
+++ Frontend/homelogin/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpResponseRedirect, HttpResponse
+from __init__ import d
 import requests
 import json
 
@@ -10,7 +11,7 @@ class Home(View):
         code = request.GET.get('code', '')
         if code != '':
             # Use the twitch supplied login code if it is present to get an access token
-            payload = {"client_id": "hkqcg5olgs65atdx08q516aqm154c9a", "client_secret": "3tmlse74r6z2yj95omk9kc3h8oah6d9", "grant_type": "authorization_code", "redirect_uri": "http://localhost:8000", "code": code}
+            payload = {"client_id": d["client_id"], "client_secret": d["client_secret"], "grant_type": "authorization_code", "redirect_uri": d["redirect_uri"], "code": code}
             token_response = requests.post('https://api.twitch.tv/kraken/oauth2/token', data=payload)
             # Use access token to get user info
             token_response_json = json.loads(token_response.text)
@@ -19,7 +20,7 @@ class Home(View):
             except KeyError:
                 return render(request, 'homelogin/home.html')
             username = requests.get('https://api.twitch.tv/kraken/user', headers=authorization_header).json()["display_name"]
-            botstatus = requests.get('http://localhost:5000/dabolinkbot/api/v1.0/bot/status/'+username).json()["online"]            
+            botstatus = requests.get(d["backend_server_ip"] + "/dabolinkbot/api/v1.0/bot/status/"+username).json()["online"]
             buttonclass = "btn-success" if botstatus else "btn-danger"
             botstatus = "Online" if botstatus else "Offline"
             return render(request, 'homelogin/home.html', {"username": username, "botstatus": botstatus, "buttonclass": buttonclass})
@@ -30,6 +31,6 @@ class Home(View):
 class Twitch(View):
     def get(self, request):
         return HttpResponseRedirect(
-            'https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=hkqcg5olgs65atdx08q516aqm154c9a&redirect_uri=http://localhost:8000&scope=user_read'
+            'https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=hkqcg5olgs65atdx08q516aqm154c9a&redirect_uri='+ d["redirect_uri"] + '&scope=user_read'
         )
         #  twitch also takes &scope= and &state=
